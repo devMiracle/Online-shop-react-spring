3990be72f8d3ea18567a5629750b6b7e5dd04117
Miracle <51058450+devMiracle@users.noreply.github.com>
2021-06-09 22:13:34 +0300

    test load state and mobx

--- broker-shop-frontend/src/index.tsx
+++ broker-shop-frontend/src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import reportWebVitals from './reportWebVitals'
+import commonStore from './stores/CommonStore'
+import { Provider } from 'mobx-react'
+
+const stores = {
+    commonStore
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider {...stores}>
+            <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ broker-shop-frontend/src/stores/CommonStore.ts
@@ -0,0 +1,42 @@
+import {action, makeObservable, observable} from "mobx"
+
+class CommonStore {
+
+    // Наблюдаемые свойства
+
+    // Флаг ожидания загрузки данных с сервера
+    @observable loading: boolean = false
+    // Текст ошибки, усли он подключен
+    @observable error: string = ''
+    //  URL REST API основной адрес
+    @observable basename: string = 'http://localhost:8080/shop/api'
+
+    constructor () {
+        makeObservable(this)
+    }
+
+    // Действия, которые могут менять значение наблюдаемых свойств
+
+    // Установка значения для флага ожидания загрузки
+    @action setLoading (loading: boolean): void {
+        this.loading = loading
+    }
+
+    @action setError (error: string): void {
+        this.error = error
+    }
+
+    @action clearError (): void {
+        this.error = ''
+    }
+}
+
+
+// Делаем доступрым для импорта из текущего модуля
+// Сам тип хранилища CommonStore
+// import { CommonStore } from 'CommonStore'
+export { CommonStore }
+// Делаем доступным для импорта из текущего модуля по умолчанию
+// Экземпляр общего хранилища типа CommonStore
+// import name from 'CommonStore'
+export default new CommonStore()
